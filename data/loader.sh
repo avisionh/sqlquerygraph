bc09641bd9805174b984f70864aecf2fd76b3033
avisionh <a_vision@hotmail.co.uk>
2021-05-26 00:30:10 +0100

    In progress

--- data/loader.sh
+++ data/loader.sh
@@ -1,6 +1,6 @@
 # enter docker neo4j instance
 docker exec -it neo4j bash
-cypher-shell -u neo4j -p "${NEO4J_PASSWORD}"
+cypher-shell --username $NEO4J_USERNAME --pasword $NEO4J_PASSWORD
 
 # use default neo4j database
 :use neo4j
