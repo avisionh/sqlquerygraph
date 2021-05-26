eb4c6d32675fb6de76755f135fa3ce812a8ac94d
avisionh <a_vision@hotmail.co.uk>
2021-05-26 07:52:56 +0100

    chore: Move data files to correct folder
    This is so we can access locally created .csv files in a READ-WRITE folder within the neo4j local container which will then be imported into the graph database.

--- data/loader.sh
+++ data/loader.sh
@@ -1,5 +1,10 @@
 # enter docker neo4j instance
 docker exec -it neo4j bash
+
+# move .csv files into import/ folder
+mv data/*csv import/
+
+# access cypher shell
 cypher-shell --username $NEO4J_USERNAME --pasword $NEO4J_PASSWORD
 
 # use default neo4j database
