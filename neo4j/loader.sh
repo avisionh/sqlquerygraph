d2d03eef441a909e7fbbe94ddad5bf6de9da3e95
avisionh <a_vision@hotmail.co.uk>
2021-06-02 23:13:54 +0100

    perf: Remove poetry install
    This is because we only need py2neo package for docker container.

--- neo4j/loader.sh
+++
@@ -1,8 +0,0 @@
-# enter docker neo4j instance
-docker exec -it neo4j bash
-
-# move .csv files into import/ folder for loading into neo4j
-mv data/*csv import/
-
-# pipe content of .cypher file into cypher-shell
-cat data/example_import.cypher | bin/cypher-shell --username $NEO4J_USERNAME --password $NEO4J_PASSWORD --format plain
